d0be5d195abc388a326facee3a50158909fb5986
sufian <sufian4se@gmail.com>
2022-10-01 00:05:22 +0600

    use rechart to draw any type of chart using react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './components/Pricing/Pricing';
 
@@ -9,6 +10,7 @@ function App() {
       <h1 className="text-3xl font-bold underline bg-orange-500 text-white py-2">Hello Tailwind</h1>
       <p className='bg-red-200 py-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum non eum ex, commodi magnam quam rerum ut suscipit officia animi modi. Similique nulla velit neque odio eius voluptas quia molestias.</p>
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
